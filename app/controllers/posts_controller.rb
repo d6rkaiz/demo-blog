9bff75df1f1e701eadd601edf369d8ed7db5269d
Isao Sugimoto <d6rkaiz@gmail.com>
2022-01-10 05:26:30 +0000

    add turbo / add turbo_stream via new post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -25,9 +25,11 @@ class PostsController < ApplicationController
 
     respond_to do |format|
       if @post.save
+        format.turbo_stream
         format.html { redirect_to post_url(@post), notice: "Post was successfully created." }
         format.json { render :show, status: :created, location: @post }
       else
+        format.turbo_stream
         format.html { render :new, status: :unprocessable_entity }
         format.json { render json: @post.errors, status: :unprocessable_entity }
       end
--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,5 @@
 module PostsHelper
+  def form_post_stream(post:)
+    turbo_stream.replace 'form', partial: 'form', locals: { post: post }
+  end
 end
