c6c8e750e8ba3b1943dcbdcf81d433b31993c3dd
Isao Sugimoto <d6rkaiz@gmail.com>
2022-01-12 09:43:12 +0000

    tweak notice / move from post/notice to application/notice

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  NOTICE = {
+    create: 'Post created successfully',
+    update: 'Post updated successfully',
+    delete: 'Post deleted successfully',
+    error: 'Something went wrong'
+  }
+
+  def notice_stream(message:, status:)
+    turbo_stream.replace 'notice', partial: 'application/notice', locals: { notice: NOTICE[message], status: status }
+  end
 end
